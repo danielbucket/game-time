abc5050f687d302f0322f413106c188e630825bd
Daniel Bucket <danielludwick@gmail.com>
2017-01-10 18:31:58 -0700

    nearly done with the ball tests. need to define "ball".

--- test/ball-test.js
+++ test/ball-test.js
@@ -1,11 +1,63 @@
 var assert = require('chai').assert
-var Ball = require("../lib/ball.js") //?
+var Ball = require("../lib/ball.js")
 
 describe('Ball', function() {
 
   it('"Ball" should be a function', function() {
     assert.isFunction(Ball)
   })
-  //test ball.scoot, should be a function
-    it('"ball.scoot" should ')
+
+    it.skip('"ball.scoot" should reverse the ball direction', function() {
+      var scoot = new Ball({
+        x: 1,
+        y: {},
+        ballRadius:10,
+        context:context,
+        canvas:{}
+      })
+      ball.scoot()
+      //not sure about the expected result,
+      //if it should equal 2
+      assert.equal(ball.dx, 2)
+    })
+
+    it.skip('"ball.scoot" should make the ball bounce off the move right', function() {
+      var scoot = new Ball({
+        x: 1,
+        y: {},
+        ballRadius:10,
+        context:context,
+        canvas:{}
+      })
+      ball.scoot()
+      //not sure about the expected result,
+      //if it should equal 2
+      asser.equal(ball.dy, 2)
+    })
+
+    it.skip('"ball.scootRevY", should reverse the ball\'s direction on the y-axis', function() {
+      var scootRevY = new Ball({
+        x: 1,
+        y: {},
+        ballRadius:10,
+        context:context,
+        canvas:{}
+      })
+      ball.scootRevY()
+      assert.equal(ball.dy, -1)
+    })
+
+    it.skip('"ball.scootRevX", should reverse the ball\'s direction on the x-axis', function() {
+      var scootRevX = new Ball({
+        x: 1,
+        y: {},
+        ballRadius:10,
+        context:context,
+        canvas:{}
+      })
+      ball.scootRevX()
+      assert.equal(ball.dx, -1)
+    })
+
+
 })
--- test/paddle-test.js
+++ test/paddle-test.js
@@ -1,5 +1,4 @@
 var assert = require('chai').assert
-
 var Paddle = require('../lib/paddle.js')
 
 describe('Paddle', function() {
@@ -11,7 +10,16 @@ describe('Paddle', function() {
     })
 
     it('"Paddle.move()" should move the paddle', function() {
-      var paddle = new Paddle({x:10, y:20, width:30, height:40, context:{}, canvas:{width:100}, lP:false, rP:true})
+      var paddle = new Paddle({
+        x:10,
+        y:20,
+        width:30,
+        height:40,
+        context:{},
+        canvas:{width:100},
+        lP:false,
+        rP:true
+      })
       paddle.move()
       assert.equal(paddle.x, 20)
     })
